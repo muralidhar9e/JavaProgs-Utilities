10a81f06a5c5b31d4e14d3c1c421dc58698a8f04
Muralidhar <muralidhar.9e@gmail.com>
2015-05-14 19:02:52 +0530

    LoadResources.java
    Loading resources file from a particular location

---
+++ LoadResources.java
@@ -0,0 +1,26 @@
+/**
+   * This method will instantiate and load a resource.
+   *
+   * @param String The name of the resource.
+   * @return Properties The resource.
+   * @throws Exception
+   */
+  public Properties loadProperties(String propertiesFilename) throws Exception
+  {
+	URL url = (new Object()).getClass().getResource("/" + propertiesFilename);
+	if (url == null)
+	{
+	  throw new Exception("Can not find " + propertiesFilename);
+	}
+	Properties props = new Properties();
+	InputStream is = url.openStream();
+	try
+	{
+	  props.load(is);
+	}
+	finally
+	{
+	  is.close();
+	}
+	return props;
+  }
